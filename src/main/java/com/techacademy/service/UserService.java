bb543b36623151ac83daa295e4f2c33d708a3439
Akane Oba <aka1105maron@gmail.com>
2024-03-05 23:52:08 +0900

    Lesson18: chapter8 User変更機能

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -2,7 +2,7 @@ package com.techacademy.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -21,10 +21,15 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
-
 }
